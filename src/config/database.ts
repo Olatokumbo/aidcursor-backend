c6c000e36e4f9b58c90cbea66ae1ef6bb517cf20
David Faith <faithodesola@gmail.com>
2022-04-18 19:06:06 +0100

    Initialize database

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+import "dotenv/config";
+
+export const AppDataSource = new DataSource({
+  type: "cockroachdb",
+  password: process.env.DATABASE_PASSWORD,
+  username: process.env.DATABASE_USERNAME,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT as unknown as number,
+  database: process.env.DATABASE,
+  ssl: true,
+});
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-import express, { Request, Response } from 'express';
-
+import express, { Request, Response } from "express";
+import { AppDataSource } from "./config/database";
 const app = express();
 
+
+// Initialize Database
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Database has been initialized");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const PORT = 8000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({
-    message: 'Hello World',
+    message: "Hello World",
   });
 });
 
 app.listen(PORT, () => {
-  console.log('Listening at PORT', PORT);
+  console.log("Listening at PORT", PORT);
 });
